adba9627aa12d9cf7c5b1374cf3e66905cccfb90
Alexandre Brayer <a.brayer@purjus.fr>
2024-12-10 20:19:03 +0100

    Update database connection to use DATABASE_DATABASE_URL_UNPOOLED and add Vercel dependency

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import { env } from '$env/dynamic/private';
-if (!env.DATABASE_POSTGRES_URL) throw new Error('DATABASE_POSTGRES_URL is not set');
-const client = neon(env.DATABASE_POSTGRES_URL);
+if (!env.DATABASE_DATABASE_URL_UNPOOLED) throw new Error('DATABASE_DATABASE_URL_UNPOOLED is not set');
+const client = neon(env.DATABASE_DATABASE_URL_UNPOOLED);
 export const db = drizzle(client);
